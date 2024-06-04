156bc441d3975ba79e5c4cd834d3d0e07db462a3
Mixafk <126934778+Mixafk@users.noreply.github.com>
2024-06-05 02:05:38 +0300

    Create dancin.js

---
+++ dancin.js
@@ -0,0 +1,17 @@
+
+var cat = document.getElementById('cat');
+
+yes_butt.onmouseover = function() {
+    cat.style.background = "url('https://i.postimg.cc/HLdH4vtF/Bongo-Cat-Transparent-PNG.gif')";
+    cat.style.backgroundSize = "285px";
+    cat.style.backgroundPosition = "-22% 130%";
+    cat.style.backgroundRepeat = "no-repeat";
+};
+    
+yes_butt.onmouseleave = function() {
+    cat.style.background = "url('https://i.postimg.cc/yxRMPKgh/Bongo-Cat-PNG-Isolated-Photo-1.png')";
+    cat.style.backgroundSize = "350px";
+    cat.style.backgroundPosition = "50% 40%";
+    cat.style.backgroundRepeat = "no-repeat";
+};
+
